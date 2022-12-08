5defb73ac020c668e3f68a9e5864580326aac6b8
Nkwor Jane <nkwor.jane.chinelo@gmail.com>
2022-12-08 15:08:43 -0800

    Making corrections

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,11 @@
-#include<stdio.h>
-
+#include <stdio.h>
 /**
- * main - -Entry point
+ * main - Entry point
  *
  * Return: 1
  */
-
 int main(void)
 {
-	fwrite("and that piece of art is useful\", - Dora Korpar, 2015-10-19\n", 61, 1);
+	fwrite(2, "add that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
 	return (1);
 }
