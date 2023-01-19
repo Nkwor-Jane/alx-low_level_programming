65e19c3cf69a4abd586bbf0ef857f90b2bb27370
Nkwor Jane <nkwor.jane.chinelo@gmail.com>
2023-01-19 06:27:40 -0800

    Task 2

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -4,12 +4,9 @@
 
 /**
  * op_add - funtction that returns addition of two intergers
- * op_sub - function that subtracts two intergers
- * op_mul - function that multiplies two integers
- * op_div - function that divides two integers
- * op_mod - modulus of two integers
  * @a: first integer
  * @b: second integer
+ * Return: sum of integers
  */
 
 
@@ -18,20 +15,48 @@ int op_add(int a, int b)
 	return (a + b);
 }
 
+/**
+ * op_sub - function to subtract two integers
+ * @a: integer 1
+ * @b: integer 2
+ * Return: value
+ */
+
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
+
+/**
+ * op_mul - function that multipleies two integers
+ * @a: integer 1
+ * @b: integer 2
+ * Return: value
+ */
 
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
 
+/**
+ * op_div - function that divides two integers
+ * @a: integer 1
+ * @b: integer 2
+ * Return: value
+ */
+
 int op_div(int a, int b)
 {
 	return (a / b);
 }
+
+/**
+ * op_mod - function that finds remainder of two integers
+ * @a: integer1
+ * @b: integer 2
+ * Return: value
+ */
 
 int op_mod(int a, int b)
 {
