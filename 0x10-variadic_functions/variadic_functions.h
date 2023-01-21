ff2e0b4e2c25adc71990600972a64869b906b8ca
Nkwor Jane <nkwor.jane.chinelo@gmail.com>
2023-01-21 15:17:49 -0800

    made some additions to the variadic_functions.c file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,4 +1,8 @@
+#ifndef PRINT_ALL
+#define PRINT_ALL
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *seperator, const unsigned int n, ...);
 void print_strings(const char *seperator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+#endif
