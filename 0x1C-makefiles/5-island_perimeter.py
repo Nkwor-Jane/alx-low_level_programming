d9313e612f4a14fc50cd751c04d2346373e10cb1
Nkwor Jane <nkwor.jane.chinelo@gmail.com>
2023-05-24 14:44:53 -0700

    Makefiles task plus Technical interview question

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+def island_perimeter(grid):
+    """
+        Function that returns perimeter of the island
+        described in grid.
+        grid is a list of integers.
+        1-water zone
+        0-land zone
+    """
+    perimeter = 0
+    R = len(grid)
+    C = len(grid[0])
+    for i in range(0, R):
+        for j in range(0, C):
+            if (grid[i][j] == 1):
+                if (grid[i-1][j] == 0):
+                    perimeter += 1
+                if (grid[i + 1][j] == 0):
+                    perimeter += 1
+                if (grid[i][j-1] == 0):
+                    perimeter += 1
+                if (grid[i][j+1] == 0):
+                    perimeter += 1
+    return perimeter
